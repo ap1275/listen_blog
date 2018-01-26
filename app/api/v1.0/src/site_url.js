ed37c829dbbd93e78df5c7bfdba8be9ac8544288
0xdead6eef <fake@mail.com>
2018-01-26 01:35:07 +0000

    add passing args from query to graphQL
    - limit max counts each queries return into 1000
    - add date range search into articles table

--- app/api/v1.0/src/site_url.js
+++ app/api/v1.0/src/site_url.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql2')
 require('dotenv').config()
 
-function exec() {
+function exec(count) {
   return new Promise(resolve => {
+    count > 1000 ? count = 1000 : count
     const c = mysql.createConnection({
       host: process.env.ND_DB_HOST,
       user: process.env.ND_DB_USER,
       password: process.env.ND_DB_PASS,
       database: process.env.ND_DB_NAME
     })
-    c.query('select title,url,created_at from site_lists where deleted_at is NULL', (e, r, f) => {
+    c.query(`select title,url,created_at from site_lists where deleted_at is NULL limit ${count}`, (e, r, f) => {
       if(e) {
         console.error(`err : ${e}`)
       } else {
